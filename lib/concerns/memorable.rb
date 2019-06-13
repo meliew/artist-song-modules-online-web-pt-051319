ce6dcc4d30d6a3f4a0dbdeaee676770950f463dd
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-13 22:45:13 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,18 +2,18 @@
 module Memorable
   module ClassMethods
 
-  def reset_all
-    all.clear
+    def reset_all
+      self.all.clear
   end
 
   def count
-    all.count
+    self.all.count
   end
 end
 
 module InstanceMethods
   def initialize
     self.class.all << self
+    end
   end
-end
 end
