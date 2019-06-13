9c91cf44abe950cd5f54fef11aa66289ae54ad9e
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-13 22:28:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Song
   extend Memorable
   extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :artist
 
@@ -32,7 +33,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
