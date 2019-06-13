d3a5cc8ea2753d17ae0fe9fe8a63504aa2b76323
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-13 22:36:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods
-  extend Findable
-  include Paramable
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
