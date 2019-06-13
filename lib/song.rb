66c38b08a9b93822ea8a0753e4db6546df0bbea9
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-13 22:22:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,10 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@all = []
+  @@songs = []
 
   def initialize
-    @@all << self
+    @@songs << self
   end
 
   # def self.find_by_name(name)
