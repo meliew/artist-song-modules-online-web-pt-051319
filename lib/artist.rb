f6a8581aa27ec167d83125552e7906fcb8d80fdf
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-13 22:46:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,16 +7,13 @@ class Artist
   include Memorable::InstanceMethods
   extend Findable
   include Paramable
-  
-
-
 
   @@artists = []
 
-  # def initialize
-  #   @@artists << self
-  #   @songs = []
-  # end
+  def initialize
+    super
+    @songs = []
+  end
 
   # def self.find_by_name(name)
   #   @@artists.detect{|a| a.name == name}
